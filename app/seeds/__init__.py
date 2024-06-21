3803783e9526b4eeb6d17f0712f8b9db546de303
Samuel <187swachira@gmail.com>
2024-06-20 21:03:16 -0400

    updates to seeders

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -12,6 +12,11 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
+        # Before seeding, truncate all tables prefixed with schema name
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        # Add a truncate command here for every table that will be seeded.
+        db.session.commit()
+        # seed_users()
         # Before seeding in production, you want to run the seed undo 
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
--- migrations/versions/20240620_204510_.py
+++ migrations/versions/20240620_204510_.py
@@ -8,6 +8,9 @@ Create Date: 2024-06-20 20:45:10.373326
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '9a71111ebd8c'
@@ -29,6 +32,9 @@ def upgrade():
     sa.UniqueConstraint('username')
     )
     # ### end Alembic commands ###
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
 
 
 def downgrade():
