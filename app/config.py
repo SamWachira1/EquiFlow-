549979dcd81deea642ef1c57dd7ed95d41c10943
Samuel <187swachira@gmail.com>
2024-07-14 16:04:28 -0400

    test2

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
 import os
 
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     FLASK_RUN_PORT = os.environ.get('FLASK_RUN_PORT')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
     # SQLAlchemy 1.4 no longer supports url strings that start with 'postgres'
     # (only 'postgresql') but heroku's postgres add-on automatically sets the
     # url in the hidden config vars to start with postgres.
@@ -12,5 +12,12 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL').replace('postgres://', 'postgresql://')
     SQLALCHEMY_ECHO = True
-    CACHE_TYPE = 'RedisCache'
+    
+    # Redis Cache Configuration
+    CACHE_TYPE = 'redis'
     CACHE_REDIS_URL = os.environ.get('REDIS_URL')
+    CACHE_REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
+    CACHE_REDIS_DB = 0
+    CACHE_REDIS_HOST = os.environ.get('REDIS_HOST')
+    CACHE_REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
+    CACHE_REDIS_TLS = os.environ.get('REDIS_USE_TLS', 'False') == 'True'
